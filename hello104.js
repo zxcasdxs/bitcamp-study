4af9bcb5b94a55ba5ca48b1e07703a0b7cdf7cd3
bsj308 <bsj308@naver.com>
2021-06-09 16:46:16 +0900

    s

--- hello104.js
+++ hello104.js
@@ -8,6 +8,8 @@ console.log("요");
 console.log("두번째");
 console.log("먼저올리기");
 console.log("----------------");
+console.log("수정");
+console.log("수정");
 
 console.log("세번째");
 
--- hello105.js
+++ hello105.js
@@ -11,7 +11,9 @@ console.log("먼저올리기");
 console.log("세번째");
 
 console.log("네번째");
+console.log("수정");
 
 console.log("다섯번째");
+console.log("수정");
 
 console.log("마지막");
