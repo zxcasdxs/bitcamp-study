039e7bab9030727a5b8a7ca852468ecdd636f8ca
zxcasdxs <zxcasdxs@gmail.com>
2021-06-09 16:43:03 +0900

    '변경2'

--- hello100.js
+++ hello100.js
@@ -2,5 +2,4 @@ console.log("hello100");
 console.log("안");
 console.log("녕");
 console.log("하");
-console.log("세");
-console.log("요");
+
--- hello202.js
+++ hello202.js
@@ -1,6 +1,6 @@
 console.log("Hello, world!");
 console.log("안녕하세요");
-console.log("test");
-console.log("bye");
 
 console.log("202 변경");
+
+console.log("변경 2번째");
