fb2121fb18e4336d225faad4b8d475c585d952a5
zxcasdxs <zxcasdxs@gmail.com>
2021-06-11 17:17:52 +0900

    '0611'

---
+++ java-basic/app/src/main/java/com/eomcs/lang/ex03/Test02.java
@@ -0,0 +1,16 @@
+package com.eomcs.lang.ex03;
+
+
+// 한글 - 리터럴
+// eng  - literal
+class Test02{
+    public static void main(String[] args) {
+        System.out.println(128);
+        System.out.println(100);
+        System.out.println(100);
+        System.out.println(100);
+
+
+
+    }
+}
