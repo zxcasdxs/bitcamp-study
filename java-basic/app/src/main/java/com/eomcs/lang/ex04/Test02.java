ca5443572f6c9d112b411ea04ea5c70051e08df3
zxcasdxs <zxcasdxs@gmail.com>
2021-06-14 18:05:03 +0900

    '0614'

---
+++ java-basic/app/src/main/java/com/eomcs/lang/ex04/Test02.java
@@ -0,0 +1,16 @@
+package com.eomcs.lang.ex04;
+
+
+public class Test02 {
+  public static void main(String[] args) {
+    char c;
+    int[] arr = new int[10];
+    double d = 0 ;
+
+    c = 0;
+    
+
+    System.out.println(arr[4]);
+    System.out.println(d);
+  }
+}
---
+++ java-basic/app/src/main/java/com/eomcs/lang/ex04/Test03.java
@@ -0,0 +1,9 @@
+package com.eomcs.lang.ex04;
+
+
+public class Test03 {
+  public static void main(String[] args) {
+    int[] arr = new int[1000];
+    
+  }
+}
