93b519e5193179a7c113691f054d51bcb2819115
zxcasdxs <zxcasdxs@gmail.com>
2021-06-23 17:26:41 +0900

    '0623'

--- java-project-01/app/src/main/java/com/eomcs/App2.java
+++ java-project-01/app/src/main/java/com/eomcs/App2.java
@@ -3,27 +3,26 @@ package com.eomcs;
 import java.util.Date;
 import java.util.Scanner;
 
-//01. main() : 프로그램의 entry point
+//01. main(): 프로그램 entry point
 //02. 사용자로부터 명령어 입력 받기
 //03. 사용자로부터 계속 명령어 입력 받기
 //04. quit 명령어를 입력 받았을 때 반복 종료하기
 //05. 명령어 별로 실행을 분기하기
-//06. 게시글 입력받기
+//06. 게시글 입력 받기
 //07. 배열을 이용하여 여러 개의 게시글 보관하기
 //08. 상수(final 변수)를 이용하여 배열의 크기를 다루기
 //09. 게시글 목록 출력하기
 //10. 게시글 상세 조회
 //11. 게시글 조회수 추가하기
 //12. 게시글 등록일 추가하기
-//13. 게시글 변경(수정)하기
+//13. 게시글 변경하기
 //14. 게시글 삭제하기
 //15. 메서드 문법을 이용하여 기능 별로 명령어를 묶어 관리하기
-//16. 클래스를 이용하여 새 데이터 타입을 정의하기
-
-
+//16. 클래스를 이용하여 새 데이터타입을 정의하기
+//17. 클래스를 이용하여 메서드를 분류하기 
 public class App2 {
 
-  //한 개의 게시글을 담을 변수를 설계
+  // 한 개의 게시글을 담을 복합 데이터의 변수를 설계
   static class Board {
     String title;
     String content;
@@ -32,38 +31,25 @@ public class App2 {
     Date createdDate;
   }
 
-  static final int BOARD_LENGTH = 5;
-
-  static Board[] boards = new Board[BOARD_LENGTH];
-  static int size = 0;
   static Scanner keyScan = new Scanner(System.in);
-
-  //  static String[] titles = new String[BOARD_LENGTH];
-  //  static String[] contents = new String[BOARD_LENGTH];
-  //  static String[] passwords = new String[BOARD_LENGTH];
-  //  static int[] viewCounts = new int[BOARD_LENGTH]; // new로 만드는 변수는 기본적으로 0으로 초기화 된다.
-  //  static Date[] createdDates = new Date[BOARD_LENGTH];
-
 
   public static void main(String[] args) {
 
+    // App 클래스에서 만든 Scanner 인스턴스를 BoardHandler 와 같이 쓴다.
+    BoardHandler.keyScan = keyScan;
 
+    System.out.println("[게시판 관리]");
 
-    System.out.println("게시판 관리");
-
-
-
-    loop : while(true) {
+    loop: while (true) {
       System.out.print("명령> ");
       String command = keyScan.nextLine();
 
       switch (command) {
-        case "list": list(); break;
-        case "add": add(); break;
-        case "update": update(); break;
-        case "delete": delete(); break;
-        case "view": view(); break;
-
+        case "list": BoardHandler.list(); break;
+        case "add": BoardHandler.add(); break;
+        case "update": BoardHandler.update(); break;
+        case "delete": BoardHandler.delete(); break;
+        case "view": BoardHandler.view(); break;
         case "quit":
           break loop;
         default:
@@ -73,118 +59,11 @@ public class App2 {
 
     keyScan.close();
 
-    System.out.println("안녕히 가세요");
-  }
-  static void list() {
-    System.out.println("[게시물 목록]");
-
-    for(int i = 0; i < size; i++) {
-      Board board = boards[i];
-      System.out.printf("%d, %s, %s, %d\n",
-          i,
-          board.title,
-          String.format("%1$tY-%1$tm-%1$td", board.createdDate),
-          board.viewCount);
-    }
+    System.out.println("안녕히 가세요!");
   }
 
-  static void add() {
-    System.out.println("[게시글 등록]");
-
-    if (size == BOARD_LENGTH) {
-      System.out.println("더 이상 게시글을 추가 할 수 없습니다.");
-      return;
-    }
-    Board board = new Board();
-
-    System.out.print("제목 : ");
-    board.title = keyScan.nextLine();
-
-    System.out.print("내용 : ");
-    board.content = keyScan.nextLine();
-
-    System.out.print("비밀번호 : ");
-    board.password = keyScan.nextLine();
-
-    board.createdDate = new Date(); //현재 날짜와 시간을 생성하여 배열에 저장한다.
-
-    size++;
-    System.out.println("게시글을 작성했습니다.");
 
 
-  }
-  static void update() {
-    System.out.println("[게시글 변경]");
-    System.out.print("번호> ");
-    int index = Integer.parseInt(keyScan.nextLine());
-
-    if(index < 0 || index >= size) {
-      System.out.println("무효한 게시글 번호입니다.");
-      return;
-    }
-
-    Board board = boards[index];
-
-    System.out.printf("제목 : (%s)? ", board.title);
-    String title = keyScan.nextLine();
-
-    System.out.printf("내용 : (%s)? ", board.content);
-    String content = keyScan.nextLine();
-
-    System.out.println("정말 변경하시겠습니까? (y/N) ");
 
 
-    if (!keyScan.nextLine().equals("y")) { //임시변수 자리에 불필요한 1회성 변수 x 메소드 삽입
-      System.out.println("변경을 취소 하였습니다.");
-      return;
-    } 
-
-    board.title = title;
-    board.content = content;
-
-    System.out.println("게시글을 변경하였습니다.");
-  }
-  static void delete() {
-    System.out.println("[게시글 삭제]");
-    System.out.print("번호> ");
-    int index = Integer.parseInt(keyScan.nextLine());
-
-    if(index < 0 || index >= size) {
-      System.out.println("무효한 게시글 번호입니다.");
-      return;
-    }
-    System.out.println("정말 변경하시겠습니까? (y/N) ");
-    if (!keyScan.nextLine().equals("y")) {
-      System.out.println("삭제를 취소 하였습니다.");
-      return;
-    } 
-
-    for(int i = index; i < size -1; i++) {
-      boards[i] = boards[i+1];
-    }
-
-    size--;
-
-    System.out.println("게시글을 삭제 하였습니다.");
-  }
-
-  static void view() {
-    System.out.println("[게시글 조회]");
-    System.out.print("번호> ");
-    int index = Integer.parseInt(keyScan.nextLine());
-
-    if(index < 0 || index >= size) {
-      System.out.println("무효한 게시글 번호입니다.");
-      return;
-    }
-
-    Board board = boards[index];
-    board.viewCount++;
-
-    System.out.printf("제목 : %s\n", board.title);
-    System.out.printf("내용 : %s\n", board.content);
-    System.out.printf("조회수 : %d\n", board.viewCount);
-    System.out.printf("등록일: %1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS\n", board.createdDate);
-
-  }
 }
